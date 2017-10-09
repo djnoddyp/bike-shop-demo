8b23b2527d110ded4fce2a68382957b8405c8c82
Patrick Nodder <patrick.nodder@tpicap.com>
2017-10-09 15:48:03 +0100

    Add post method to controller

--- src/main/java/pnodder/controllers/WelcomeController.java
+++ src/main/java/pnodder/controllers/WelcomeController.java
@@ -2,15 +2,28 @@ package pnodder.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pnodder.model.Resident;
 
 @Controller
 public class WelcomeController {
 
+    @ModelAttribute
+    public void populateModel(Model model) {
+        model.addAttribute("message", "Yo");
+        model.addAttribute("resident", new Resident());
+    }
+
     @RequestMapping(method = RequestMethod.GET, path = "/welcome")
-    public String getWelcome(Model model) {
-        model.addAttribute("message", "Yo what's happening?");
+    public String getWelcome() {
+        return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/saveResident")
+    public String saveResident(Resident resident) {
+        System.out.println("Created: " + resident.getName() + " " + resident.getAddress());
         return "index";
     }
 
